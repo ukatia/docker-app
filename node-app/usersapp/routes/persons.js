5e3c251fb5fe912483eee3e8101ec43ab8d2edf7
ukatia <esc.katia@gmail.com>
2022-03-26 14:11:27 +0200

    Create nodejs application

---
+++ node-app/usersapp/routes/persons.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../database');
+
+// List all persons
+router.get("/all", function(req, res) {
+    db.Person.findAll()
+        .then( persons => {
+            res.status(200).send(JSON.stringify(persons));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+// Get person by id
+router.get("/:id", function(req, res) {
+    db.Person.findByPk(req.params.id)
+        .then( person => {
+            res.status(200).send(JSON.stringify(person));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+// Create person
+router.put("/", function(req, res) {
+    db.Person.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        id: req.body.id
+        })
+        .then( person => {
+            res.status(200).send(JSON.stringify(person));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+// Delete person by id
+router.delete("/:id", function(req, res) {
+    db.Person.destroy({
+        where: {
+            id: req.params.id
+        }
+        })
+        .then( () => {
+            res.status(200).send();
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+module.exports = router;
